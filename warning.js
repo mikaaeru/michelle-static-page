9235075496c92b9bbeaae7d82537a580b928db4e
Michelle S. W. <sel@kamikami.eu>
2026-03-03 10:42:17 +0700

    fix

--- warning.js
+++ warning.js
@@ -163,7 +163,7 @@
                     <button id="accept-btn" class="mc-btn" disabled>INITIALIZING</button>
                 </div>
             </div>
-            <button id="blog-bar-btn" class="blog-bar-btn">➜ VISIT BLOG NOW!!!!!  (NEWLY ADDED FEATURE)</button>
+            <button id="blog-bar-btn" class="blog-bar-btn">➜ VISIT BLOG NOW!  (NEWLY ADDED FEATURE)</button>
         </div>
     `;
     document.body.appendChild(consentOverlay);
